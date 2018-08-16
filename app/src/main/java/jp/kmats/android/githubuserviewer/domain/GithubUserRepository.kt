1f9fc504dc2677ff3eb7e9f98f1b5eace55ad591
k-mats <gfeenhaftsylphid@gmail.com>
2018-08-16 20:22:21 +0900

    Set the default lastSeenNumericId to 0
    because it is equal to no `since` parameter.

--- app/src/main/java/jp/kmats/android/githubuserviewer/domain/GithubUserRepository.kt
+++ app/src/main/java/jp/kmats/android/githubuserviewer/domain/GithubUserRepository.kt
@@ -7,5 +7,11 @@ interface GithubUserRepository {
 
     fun getGithubUser(loginId: String): Observable<GithubUser>
 
-    fun getGithubUserList(lastSeenNumericId: Long): Observable<List<GithubUser>>
+    /**
+     * The "last seen numeric ID 0" equals to no `since` parameter.
+     * Thus both urls below returns the same json.
+     * - https://api.github.com/users
+     * - https://api.github.com/users?since=0
+     */
+    fun getGithubUserList(lastSeenNumericId: Long = 0): Observable<List<GithubUser>>
 }
