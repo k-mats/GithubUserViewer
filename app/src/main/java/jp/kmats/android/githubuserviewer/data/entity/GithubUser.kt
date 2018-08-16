4bbae811c76f77d678e4419e0e0cbb7e1983d141
k-mats <gfeenhaftsylphid@gmail.com>
2018-08-17 00:08:12 +0900

    Implement user list cardview design

--- app/src/main/java/jp/kmats/android/githubuserviewer/data/entity/GithubUser.kt
+++ app/src/main/java/jp/kmats/android/githubuserviewer/data/entity/GithubUser.kt
@@ -5,7 +5,7 @@ import com.squareup.moshi.Json
 
 data class GithubUser(
         @Json(name = "login") val loginId: String,
-        @Json(name = "id") val numericId: Long,
+        @Json(name = "id") val numericalId: Long,
         @Json(name = "avatar_url") val avatarUrl: Uri,
         val name: String?,
         val company: String?,
--- app/src/main/java/jp/kmats/android/githubuserviewer/presentation/list/GithubUserAdapter.kt
+++ app/src/main/java/jp/kmats/android/githubuserviewer/presentation/list/GithubUserAdapter.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import jp.kmats.android.githubuserviewer.R
@@ -24,11 +25,19 @@ class GithubUserAdapter(private val users: List<GithubUser>, private val context
 
     override fun onBindViewHolder(holder: CardViewHolder, position: Int) {
         val user = users[position]
+
         val glideOptions = RequestOptions().circleCrop().placeholder(R.drawable.avatar_placeholder)
         Glide.with(context).load(user.avatarUrl).apply(glideOptions).into(holder.avatarImageView)
+
+        holder.loginIdTextView.text = user.loginId
+
+        val idText = context.resources.getString(R.string.cardview_id_text)
+        holder.numericalIdTextView.text = String.format(idText, user.numericalId)
     }
 
     class CardViewHolder(cardView: View) : RecyclerView.ViewHolder(cardView) {
         val avatarImageView: ImageView = cardView.avatarImageView
+        val loginIdTextView: TextView = cardView.loginIdTextView
+        val numericalIdTextView: TextView = cardView.numericalIdTextView
     }
 }
