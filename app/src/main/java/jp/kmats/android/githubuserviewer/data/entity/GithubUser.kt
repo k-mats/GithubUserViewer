84cd5451bbd28fe7358b0919310c31f52c9c1ebe
k-mats <gfeenhaftsylphid@gmail.com>
2018-08-16 18:34:32 +0900

    Create Github user entity

---
+++ app/src/main/java/jp/kmats/android/githubuserviewer/data/entity/GithubUser.kt
@@ -0,0 +1,19 @@
+package jp.kmats.android.githubuserviewer.data.entity
+
+import android.net.Uri
+import com.squareup.moshi.Json
+
+data class GithubUser(
+        @Json(name = "login") val loginId: String,
+        @Json(name = "avatar_url") val avatarUrl: Uri,
+        val name: String?,
+        val company: String?,
+        val blog: String?,
+        val location: String?,
+        val email: String?,
+        val hireable: Boolean?,
+        val bio: String?,
+        @Json(name = "public_repos") val publicRepos: Int?,
+        @Json(name = "public_gists") val publicGists: Int?,
+        val followers: Int?,
+        val following: Int?)
